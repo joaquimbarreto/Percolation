c88e1a6b6c731cbb32507c89c1066193e5244eb3
Joaquim Barreto <joaquim@barreto.co>
2020-03-26 17:07:59 +0100

    library added

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,5 +1,23 @@
 public class Percolation {
-    public static void main(String[] args) {
-        System.out.println("Percolation, Baby!!!!");
-    }
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n){}
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col){}
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col){}
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col){}
+
+    // returns the number of open sites
+    public int numberOfOpenSites(){}
+
+    // does the system percolate?
+    public boolean percolates(){}
+
+    // test client (optional)
+    public static void main(String[] args){}
 }
